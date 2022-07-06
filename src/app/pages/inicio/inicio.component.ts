85fcca712f4bb7ebb0974de8ace9186a340fa08d
carlos <csalas1285@gmail.com>
2022-07-06 00:21:05 -0500

    correcciones maqueta

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -18,5 +18,30 @@ export class InicioComponent implements OnInit {
 
   }
 
+  mostrarPaso1 = true;
+  mostrarPaso2 = false;
+  mostrarPaso3 = false;
 
+  mostrarOcultar(paso: number){
+   switch(paso){
+case 1:
+  this.mostrarPaso1 = true;
+  this.mostrarPaso2 = false;
+  this.mostrarPaso3 = false;
+  break;
+  case 2: 
+  this.mostrarPaso1 = false;
+  this.mostrarPaso2 = true;
+  this.mostrarPaso3 = false;
+  break;
+  case 3: 
+  this.mostrarPaso1 = false;
+  this.mostrarPaso2 = false;
+  this.mostrarPaso3 = true;
+  break;
+  default:
+    this.mostrarPaso1 = true;
+   }
+  }
 }
+
